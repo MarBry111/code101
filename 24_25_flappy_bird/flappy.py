cf0098916cb14ed4c3b2d1023eeee5d306dc3d12
MarBry111 <marek.brynda96@gmail.com>
2025-01-18 08:14:46 +0100

    Dodanie flappy bird week 1 + zadania week 1 w oddzielnym pliku

--- 24_25_flappy_bird/flappy.py
+++ 24_25_flappy_bird/flappy.py
@@ -1,65 +1,84 @@
-# To jest nasz pierwszy program w Pygame!
-# Będziemy tworzyć grę Flappy Bird, ale zaczniemy od podstaw
-
-# ============= Krok 1 =============
-# Najpierw musimy zaimportować potrzebne biblioteki
-# pygame - do tworzenia gry
 import pygame
 
-# ============= Krok 2 =============
-# Musimy włączyć silnik Pygame
-# To jak włączenie konsoli do gier!
+# Włączamy silnik Pygame
 pygame.init()
 
-# ============= Krok 3 =============
-# Ustawiamy rozmiar okna naszej gry
-SZEROKOSC = 300
-WYSOKOSC = 300
+# Ustawiamy rozmiar okna naszej gry i FPSy
+SZEROKOSC = 400
+WYSOKOSC = 600
 FPS = 60
 
 # Zegar gry
 zegar = pygame.time.Clock()
 
-# ============= Krok 4 =============
-# Tworzymy okno gry
+# Tworzymy okno gry, gdzie wszystko będziemy wyświetlać
 ekran = pygame.display.set_mode((SZEROKOSC, WYSOKOSC))
 
 # Ustawiamy tytuł okna
-pygame.display.set_caption('Moja Super Gra!')
+pygame.display.set_caption('Flappy Bird')
 
 # ============= Krok 5 =============
 # Wybieramy kolory, romziar i połozenie Flappiego
-# Kolory w Pygame to trzy liczby: (R, G, B)
-# Każda liczba może być od 0 do 255
+# Kolory w Pygame to trzy liczby: (R, G, B) z zakresu 0-255
 BIALY = (255, 255, 255) 
-NIEBIESKI = (0, 0, 255)
+CZARNY = (0, 0 , 0)
 
-FLAPPY_Y = 150
-FLAPPY_X = 150
+NIEBIESKI = (0, 0, 255)
+ZOTLY = (255, 255, 0)
+
 FLAPPY_ROZMIAR = 40
+
+# ustawiamy flappiego na środku ekranu
+# odpowiednio wysokość/2 - połowa flappiego, i szerokosć/2 - połowa flappiego
+FLAPPY_Y = WYSOKOSC // 2 - FLAPPY_ROZMIAR // 2
+FLAPPY_X = SZEROKOSC // 2 - FLAPPY_ROZMIAR // 2
+
+# początkowa prędkosć i stałe związane z prędkością
+predkosc = 1
+MAX_PREDKOSC = 5
+SILA_SKOKU = 60  # o ile do góry skoczy FLAPPY
+PRZYSPIESZENIE = 0.1
 
 # ============= Krok 6 =============
 # Główna pętla gry
-# To jak filmik, który pokazuje każdą klatkę po kolei
 gra_dziala = True
+
 while gra_dziala:
     # -------- Sprawdzamy wydarzenia (np. kliknięcia) --------
     for event in pygame.event.get():
         # Jeśli ktoś kliknie X na oknie
         if event.type == pygame.QUIT:
-            # Kończymy grę
             gra_dziala = False
-    
+        # po naciśnieciu SPACJI
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            # przesuń flappiego do góry o siłę skoku
+            FLAPPY_Y = FLAPPY_Y - SILA_SKOKU
+            # wyseruj prędkosć spadania
+            predkosc = 0
+
+    # sprawdź czy prędkosć przekroczyła wartosć krytyczną
+    if predkosc < MAX_PREDKOSC:
+        # jeśli nie to przyspieszaj
+        predkosc = predkosc + PRZYSPIESZENIE
+    else: 
+        # jeśli tak to stała prędkosć
+        predkosc = MAX_PREDKOSC
+
+    # Sprawdź czy flappy dotyka dolnej krawędzi ekranu
+    if FLAPPY_Y >= WYSOKOSC - FLAPPY_ROZMIAR:
+        FLAPPY_Y = WYSOKOSC - FLAPPY_ROZMIAR
+    else:
+        # aktualizuj ruch flappiego jesli na ekranie
+        FLAPPY_Y = FLAPPY_Y + predkosc
+
+
     # -------- Rysujemy na ekranie --------
-    # Najpierw wypełniamy cały ekran białym kolorem
-    ekran.fill(BIALY)
-    
+    ekran.fill(CZARNY)
+
     # Rysujemy niebieski kwadrat na środku
-    # Parametry to: (gdzie, kolor, (x, y, szerokość, wysokość))
-    pygame.draw.rect(ekran, NIEBIESKI, (FLAPPY_X, FLAPPY_Y, FLAPPY_ROZMIAR, FLAPPY_ROZMIAR))
+    pygame.draw.rect(ekran, ZOTLY, (FLAPPY_X, FLAPPY_Y, FLAPPY_ROZMIAR, FLAPPY_ROZMIAR))
     
     # -------- Pokazujemy wszystko na ekranie --------
-    # To jak pokazanie gotowego rysunku
     pygame.display.update()
     zegar.tick(FPS)
 
@@ -68,12 +87,3 @@ while gra_dziala:
 pygame.quit()
 quit()
 
-# ============= ZADANIA DO ZROBIENIA =============
-# 1. Zmień rozmiar okna
-# 2. Dodaj inne kolory - spóbuj dostać żółty
-# 3. Przesuń Flappiego w inne miejsce
-# 4. Ustaw Flappiego na środku ekranu (czy obie zmienne będa się zmieniać - X i Y?)
-# 5. Spraw żeby kwadrat poruszał się w dół
-# 6. Spraw żeby kwadrat przyspieszał w dół (poruszał się coraz szybciej i jak dotrze na dół  - przenieś go na górę)
-# 7. Spraw żeby spacja powodowała "skok" flappiego
-# 8. Kiedy Flappy dotrze na górę albo na dół zatrzymaj go (ma przestać sie poruszać i nie wystawać poza ekran)
