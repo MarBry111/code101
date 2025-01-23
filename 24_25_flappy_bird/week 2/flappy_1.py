0b519c8a9060ba357f325ae34288d29ee98cb790
MarBry111 <marek.brynda96@gmail.com>
2025-01-23 08:34:50 +0100

    Podzia zada z week 2 na week 2 i week 3 + update kodu flappiego

--- 24_25_flappy_bird/week 2/flappy_1.py
+++ 24_25_flappy_bird/week 2/flappy_1.py
@@ -4,7 +4,7 @@ import pygame
 pygame.init()
 
 # Ustawiamy rozmiar okna naszej gry i FPSy
-SZEROKOSC = 600
+SZEROKOSC = 400
 WYSOKOSC = 600
 FPS = 60
 
@@ -17,6 +17,7 @@ ekran = pygame.display.set_mode((SZEROKOSC, WYSOKOSC))
 # Ustawiamy tytuł okna
 pygame.display.set_caption('Flappy Bird')
 
+# ============= Krok 5 =============
 # Wybieramy kolory, romziar i połozenie Flappiego
 # Kolory w Pygame to trzy liczby: (R, G, B) z zakresu 0-255
 BIALY = (255, 255, 255) 
@@ -24,7 +25,6 @@ CZARNY = (0, 0 , 0)
 
 NIEBIESKI = (0, 0, 255)
 ZOTLY = (255, 255, 0)
-ZIELONY = (0, 255, 0)
 
 FLAPPY_ROZMIAR = 40
 
@@ -35,16 +35,11 @@ FLAPPY_X = SZEROKOSC // 2 - FLAPPY_ROZMIAR // 2
 
 # początkowa prędkosć i stałe związane z prędkością
 predkosc = 1
+MAX_PREDKOSC = 5
 SILA_SKOKU = 60  # o ile do góry skoczy FLAPPY
 PRZYSPIESZENIE = 0.1
 
-# zmienne rury
-RURA_Y = 0
-RURA_X = SZEROKOSC
-RURA_SZEROKOSC = 50
-RURA_WYSOKOSC = WYSOKOSC
-RURA_PREDKOSC = 5
-
+# ============= Krok 6 =============
 # Główna pętla gry
 gra_dziala = True
 
@@ -61,36 +56,34 @@ while gra_dziala:
             # wyseruj prędkosć spadania
             predkosc = 0
 
+    # sprawdź czy prędkosć przekroczyła wartosć krytyczną
+    if predkosc < MAX_PREDKOSC:
+        # jeśli nie to przyspieszaj
+        predkosc = predkosc + PRZYSPIESZENIE
+    else: 
+        # jeśli tak to stała prędkosć
+        predkosc = MAX_PREDKOSC
+
     # Sprawdź czy flappy dotyka dolnej krawędzi ekranu
     if FLAPPY_Y >= WYSOKOSC - FLAPPY_ROZMIAR:
         FLAPPY_Y = WYSOKOSC - FLAPPY_ROZMIAR
-        predkosc = 0
-    elif FLAPPY_Y <= 0:
-        FLAPPY_Y = 0
-        predkosc = 0
+    else:
+        # aktualizuj ruch flappiego jesli na ekranie
+        FLAPPY_Y = FLAPPY_Y + predkosc
 
-    # porusznie flappim
-    predkosc = predkosc + PRZYSPIESZENIE
-    FLAPPY_Y = FLAPPY_Y + predkosc
-
-    # poruszanie rury
-    RURA_X = RURA_X - RURA_PREDKOSC
-    # kiedy rura poza lewym fregmentem ekranu wtedy przenieś na prawo
-    if RURA_X <= 0:
-        RURA_X = SZEROKOSC
 
     # -------- Rysujemy na ekranie --------
     ekran.fill(CZARNY)
 
-    # Rysujemy flappiego
+    # Rysujemy niebieski kwadrat na środku
     pygame.draw.rect(ekran, ZOTLY, (FLAPPY_X, FLAPPY_Y, FLAPPY_ROZMIAR, FLAPPY_ROZMIAR))
-    # Rysujemy rurę
-    pygame.draw.rect(ekran, ZIELONY, (RURA_X, RURA_Y, RURA_SZEROKOSC, RURA_WYSOKOSC))
-
+    
     # -------- Pokazujemy wszystko na ekranie --------
     pygame.display.update()
     zegar.tick(FPS)
 
+# ============= Krok 7 =============
+# Sprzątamy po sobie (wyłączamy Pygame)
 pygame.quit()
 quit()
 
