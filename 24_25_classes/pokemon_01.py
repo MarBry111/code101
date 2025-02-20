c9e4d308b6ae2530e21c4a2e8c8a60d0f3067b4c
MarBry111 <marek.brynda96@gmail.com>
2025-02-20 10:31:42 +0100

    Dodanie przykladu klas na pokemonach

---
+++ 24_25_classes/pokemon_01.py
@@ -0,0 +1,15 @@
+imie = "Pikachu"
+typ = "Elektryczny"
+level = 5
+
+# przedstaw sie
+print(f"To jest {imie}, typ: {typ}, poziom: {level}")
+
+# leveluj
+level = level + 1
+print(f"{imie} awansował na poziom {level}!")
+
+# atakuj
+print(f"{imie} używa ataku!")
+
+# 3 inne pokemony, leveluj je, przedstaw i użyj ataku
