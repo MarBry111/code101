725b4b084183811637ff82dd02c48a744112401a
MarBry111 <marek.brynda96@gmail.com>
2026-05-14 18:48:38 +0200

    Add backroom escape 01

---
+++ 25_26_INSERT/06_klasy/01_player/gra.py
@@ -0,0 +1,27 @@
+import pygame
+from player import Player
+from pomoc import rysuj_poziom, ustaw_ekran
+from levels import LEVEL_1
+
+pygame.init()
+
+level = LEVEL_1
+screen = ustaw_ekran(level)
+pygame.display.set_caption("Backrooms Escape")
+
+player = Player(x=1, y=1)
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    rysuj_poziom(screen, level)
+    player.rysuj(screen)
+
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
